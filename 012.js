ddbe9d3a44febaae8af5b0efbe260973eebc0039
ThisisMiri <mirikim715@gmail.com>
2022-08-06 22:45:00 +0900

    011 012

--- 012.js
+++ 012.js
@@ -6,9 +6,9 @@
   /* <여기에 class를 작성하세요.> */
 }
 
-const x = new Wizard(545, 210, 10);
-console.log(x.health, x.mana, x.armor);
-x.attack();
+// const x = new Wizard(545, 210, 10);
+// console.log(x.health, x.mana, x.armor);
+// x.attack();
 
 // 출력
 // 545 210 10
