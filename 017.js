a15f70a231df2f89d9d87142e5b9c0f6b2849641
ThisisMiri <mirikim715@gmail.com>
2022-08-10 22:45:00 +0900

    017

--- 017.js
+++ 017.js
@@ -3,3 +3,11 @@
 
 // 입력으로 키가 주어지면
 // 키가 150이 넘으면 YES를 틀리면 NO를 출력하는 프로그램을 작성하세요.
+
+const height = prompt("키를 입력하세요.");
+
+if (height >= 150) {
+  console.log("YES");
+} else {
+  console.log("NO");
+}
