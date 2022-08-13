77f4b71a5bca35caef7f467e49dc77f335a3df68
ThisisMiri <mirikim715@gmail.com>
2022-08-13 22:45:00 +0900

    018

--- 018.js
+++ 018.js
@@ -2,3 +2,12 @@
 
 // 공백으로 구분하여 세 과목의 점수가 주어지면 전체 평균 점수를 구하는 프로그램을 작성하세요.
 // 단, 소숫점 자리는 모두 버립니다.
+
+const scores = prompt("세 과목의 점수를 입력하세요.").split(" ");
+let sum = 0;
+
+for (let i = 0; i < 3; i++) {
+  sum += parseInt(scores[i], 10);
+}
+
+console.log(Math.floor(sum / 3));
