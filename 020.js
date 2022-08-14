ffa807c203a10c71afd87044e592755922d6f9b2
ThisisMiri <mirikim715@gmail.com>
2022-08-14 22:45:00 +0900

    020

--- 020.js
+++ 020.js
@@ -5,3 +5,10 @@
 
 // 입력 : 10 2
 // 출력 : 5 0
+
+const n = prompt("수를 입력하세요.").split(" ");
+
+const result = Math.floor(parseInt(n[0], 10) / parseInt(n[1], 10));
+const left = parseInt(n[0], 10) % parseInt(n[1], 10);
+
+console.log(result, left);
