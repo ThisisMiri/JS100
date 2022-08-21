9f3eb8b8e41311c5cc6a10968fa7ccbc394f78fa
ThisisMiri <mirikim715@gmail.com>
2022-08-21 22:45:00 +0900

    025

--- 025.js
+++ 025.js
@@ -2,3 +2,13 @@
 // 함수를 사용하여 원의 넓이를 구하는 코드를 작성해봅시다.
 
 // 입력으로 반지름의 길이 정수 n이 주어지면 원의 넓이를 반환하는 함수를 만들어 주세요.
+
+function circle(n) {
+  const result = n * n * 3.14;
+
+  return result;
+}
+
+const r = prompt("원의 반지름을 입력하세요.");
+
+console.log(circle(r));
