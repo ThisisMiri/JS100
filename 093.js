a2ad4d757daebbb3d66e4b88c946b5b5bd2ee641
ThisisMiri <mirikim715@gmail.com>
2022-11-26 10:38:00 +0900

    093

---
+++ 093.js
@@ -0,0 +1,45 @@
+/*
+페이지 교체 알고리즘은 메모리를 관리하는 운영체제에서, 페이지 부재가 발생하여 새로운 페이지를 할당하기 위해 현재 **할당된 페이지 중 어느 것을 교체할지를 결정하는 방법**입니다.
+이 알고리즘이 사용되는 시기는 페이지 부재(Page Fault)가 발생해 새로운 페이지를 적재해야 하지만 페이지를 적재할 공간이 없어 이미 적재되어 있는 페이지 중 교체할 페이지를 정할 때 사용됩니다. 빈 페이지가 없는 상황에서 메모리에 적재된 페이지와 적재할 페이지를 교체함으로 페이지 부재 문제를 해결할 수 있습니다.
+
+이 중 선입선출(FIFO) 알고리즘은 가장 먼저 들어와서 가장 오래있었던 페이지를 우선으로 교체시키는 방법을 의미합니다. 아래의 그림을 참고해주세요.
+
+메모리의 크기가 i로 주어지고 들어올 페이지들이 n으로 주어졌을 때, 전체 실행시간을 구해주세요.
+
+만약 스택 안에 같은 스케줄이 있다면 hit 이라고 하며 실행시간은 1초 입니다. 스택 안에 스케줄이 없다면 miss 라고 하며 실행시간은 6초 입니다.
+*/
+
+function solution(frame, page) {
+  let runTime = 0;
+  let temp = [];
+
+  // frame이 0일때, page의 크기만큼 6을 곱해주고 끝낸다
+  if (frame === 0) {
+    runTime = page.length * 6;
+    return runTime;
+  }
+
+  for (let i of page) {
+    if (temp.includes(i)) {
+      runTime += 1;
+    } else {
+      // 배열이 비었으면 무조건 넣어야 하므로 if문을 사용
+      if (temp.length < frame) {
+        temp.push(i);
+      } else {
+        // 가장 사용되지 않은 첫번째 배열을 제거하고 맨 뒤에 입력값을 저장
+        temp.shift();
+        temp.push(i);
+      }
+
+      // if문 실행에 상관없이 runTime은 10이 추가된다
+      runTime += 6;
+    }
+  }
+  return runTime;
+}
+
+const f = parseInt(prompt("프레임을 입력해주세요."), 10);
+const page = prompt("페이지를 입력해주세요.").split("");
+
+console.log(solution(f, page));
